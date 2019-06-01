77929b5881c38414c0ae7b822ce545e36321d885
WheatleyTheCore <alex.colwell@icloud.com>
2019-06-01 17:44:29 -0400

    abstracted navbar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,48 +1,14 @@
 import React from 'react'
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText'
-import Link from 'react-router-dom/Link'
-import './Home.css'
+import './Home.css';
+import Navbar from '../Navbar/Navbar';
+
+
 
 const Home = () => {
-    const [state, setState] = React.useState({
-        isOpen: false                          //state that determines if drawer is open
-    })
-    const toggleDrawer = () => {             //function for toggling wether or not drawer is open
-        setState({ isOpen: !state.isOpen })
-        console.log(state.isOpen)
-    }
-
-    const navList = () => {                  //creates list of router links
-        return (
-            <div>
-                <List>
-                    {
-                        ['asdf', 'fda', 'hello'].map((text) => (  //iterates over array of link names to make a list of links
-                            <Link to={'/' + text} className='button'>
-                                <ListItem button key={text}>
-                                    <ListItemText primary={text} />
-                                </ListItem> 
-                            </Link>
-                        ))
-                    }
-                </List>
-            </div>
-        )                  
-    }
-
     return (
         <div>
-            <Button onClick={toggleDrawer}>open</Button>
-            <div>hi</div>
-            <SwipeableDrawer
-                anchor='left'
-                open={state.isOpen}
-                onOpen={toggleDrawer}
-                onClose={toggleDrawer}>{navList()}</SwipeableDrawer>
+            <Navbar />
+            <h1>hello</h1>
         </div>
     )
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText'
+import Link from 'react-router-dom/Link'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+
+
+
+const Navbar = () => {
+    const [state, setState] = React.useState({
+        isOpen: false                          //state that determines if drawer is open
+    })
+    const toggleDrawer = () => {             //function for toggling wether or not drawer is open
+        setState({ isOpen: !state.isOpen })
+        console.log(state.isOpen)
+    }
+
+    const navList = () => {                  //creates list of router links
+        return (
+            <div>
+                <List>
+                    {
+                        ['asdf', 'fda', 'hello'].map((text) => (  //iterates over array of link names to make a list of links
+                            <Link to={'/' + text} className='button'>
+                                <ListItem button key={text}>
+                                    <ListItemText primary={text} />
+                                </ListItem>
+                            </Link>
+                        ))
+                    }
+                </List>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <AppBar position='static'>
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="Menu" onClick={() => {
+                        toggleDrawer()
+                    }}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant='h6'>Teach</Typography>
+                    <SwipeableDrawer
+                        anchor='right'
+                        open={state.isOpen}
+                        onOpen={toggleDrawer}
+                        onClose={toggleDrawer}>{navList()}</SwipeableDrawer>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Navbar
