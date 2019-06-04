8c44cacce1cda731598b5f020fdf081f49189935
WheatleyTheCore <alex.colwell@icloud.com>
2019-06-04 15:09:11 -0400

    cleaned up some code

--- src/Components/Navbar/Navlist.js
+++ src/Components/Navbar/Navlist.js
@@ -4,12 +4,13 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText'
 import Link from 'react-router-dom/Link'
 
+
 const NavList = () => {                  //creates list of router links
     return (
         <div>
             <List>
                 {
-                    ['asdf', 'fda', 'hello'].map((text) => (  //iterates over array of link names to make a list of links
+                    ['Chemistry', 'Latin', 'Math'].map((text) => (  //iterates over array of link names to make a list of links
                         <Link to={'/' + text} className='button'>
                             <ListItem button key={text}>
                                 <ListItemText primary={text} />
