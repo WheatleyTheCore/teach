1649de22cef1424e92e8872b7a214037929ab06f
WheatleyTheCore <alex.colwell@icloud.com>
2019-07-06 12:40:22 -0400

    added match parameters so that individual lesson and welcome components are unnecessary

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
-import ChemWelcome from './Courses/Chemistry/ChemWelcome/ChemWelcome';
 import Lesson from './Components/Lesson/Lesson';
 import WelcomeCard from './Components/WelcomeCard/WelcomeCard';
 
--- src/Components/Lesson/Lesson.js
+++ src/Components/Lesson/Lesson.js
@@ -1,12 +1,13 @@
 import React from "react"
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import './Lesson.css'
 
 const Lesson = ({match}) => {
     let jsonFile = require(`../../Courses/${match.params.course}/lessons/${match.params.lesson}.json`)
    
     return (
-        <div>
+        <div id="Lesson">
             <Paper>
                 <Typography variant='h1'>
                     {jsonFile.title}
